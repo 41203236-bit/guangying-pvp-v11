2b15a0e7bca0542698e2447806f94c5806d0c229
41203236-bit <41203236@gcloud.csu.edu.tw>
2026-04-02 13:52:41 +0800

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: '請填入你的 apiKey',
+  authDomain: '請填入你的 authDomain',
+  databaseURL: '請填入你的 databaseURL',
+  projectId: '請填入你的 projectId',
+  storageBucket: '請填入你的 storageBucket',
+  messagingSenderId: '請填入你的 messagingSenderId',
+  appId: '請填入你的 appId'
+};
